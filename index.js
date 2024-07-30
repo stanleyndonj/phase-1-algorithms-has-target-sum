c0956087409e774b02cb59b7ac766226a9afd0e1
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-07-30 23:48:11 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+    const numMap = {};
+
+    for (let i = 0; i < array.length; i++) {
+        const complement = target - array[i];
+
+        if (numMap[complement]) {
+            return true;
+        }
+
+        numMap[array[i]] = true;
+    }
+
+    return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+  O(n) because the hash table allows us to perform constant-time lookups for each element.
+
 */
 
 /* 
   Add your pseudocode here
+
+  initialize an empty hash table
+    loop through array elements
+      initalize complement variable that hold target - array element of [i]
+        if hash table contains complement
+         return true
+
+        if hash table does not contain complement add the complement to the hash table
+        end loop
+
+    if there is no pair can be found 
+      return false
+
+      end of function
 */
 
 /*
   Add written explanation of your solution here
+1. We initialize an empty hash table numMap.
+2. We iterate through the array once.
+3. For each element array[i], we calculate its complement by subtracting it from the target.
+4. If the complement exists in the hash table, it means we've found two elements that sum up to the target, so we return true.
+5. If the complement doesn't exist in the hash table, we add the current element array[i] to the hash table.
+6. If we finish iterating through the array without finding a pair, we return false
 */
 
 // You can run `node index.js` to view these console logs
